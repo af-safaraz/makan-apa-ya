708d2727d588a67446041c5bdec38d8423752a61
Andi Fikri Safaraz <afsafarazd@gmail.com>
2024-11-30 17:09:42 +0800

    Fix height container on mobile and wheel page for desktop

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center 
         sm:flex-row sm:h-full">
             <div className="pb-5 h-[60vh] flex flex-col justify-end items-center text-center 
-            sm:pl-8 sm:pr-3 sm:pb-0 sm:justify-center sm:items-start sm:w-[55%] sm:h-full sm:min-h-screen sm:text-left">
-                <h1 className="font-dela-gothic text-6xl text-primary text-black-border text-black-shadow-sm leading-[1.15] sm:text-7xl sm:text-black-shadow-lg">
+            sm:pl-8 lg:pl-10 xl:pl-12 sm:pr-4 sm:pb-0 sm:justify-center sm:items-start sm:w-[55%] sm:text-left sm:h-full">
+                <h1 className="font-dela-gothic text-6xl text-primary text-black-border text-black-shadow-sm leading-[1.15] sm:text-6xl lg:text-7xl sm:text-black-shadow-lg">
                     Makan <span className="xl:inline block">Apa Ya?</span>
                 </h1>
-                <p className="my-7 px-3 font-poppins text-sm text-gray-700 sm:text-lg">
+                <p className="my-7 font-poppins text-sm text-gray-700 sm:text-lg">
                     Bingung mau makan apa?
                     <span className="lg:inline block"> Biarkan kami bantu pilih!</span>
                     <span className="block">Masukkan pilihan makan favoritmu atau pilih dari daftar kami, lalu putar roda untuk mendapatkan jawaban. Mudah, cepat, dan tanpa drama!</span>
--- src/sections/Wheel.jsx
+++ src/sections/Wheel.jsx
@@ -2,22 +2,22 @@ import Button from '../components/Button'
 
 const Wheel = () => {
     return (
-        <div className="flex flex-col">
-            <div className="h-[50vh] flex justify-center items-center p-5">
+        <div className="flex flex-col sm:flex-row w-full lg:w-[90%]">
+            <div className="h-[50vh] flex justify-center items-center p-5 sm:w-[50%] sm:h-screen">
                 <div className="wheel-container w-full h-full bg-primary"></div>
             </div>
-            <div className="min-h-[50vh] flex flex-col items-center">
-                <h6 className="font-poppins font-medium text-gray-700">Daftar Makanan:</h6>
-                <textarea id="foodList" name="foodList" rows="6" placeholder="Masukkan nama-nama makanan yang dipisahkan dengan baris baru seperti:&#10;Nasi Goreng&#10;Bakso&#10;Sate&#10;Soto" className="my-3 p-2 w-[80%] font-poppins text-sm text-gray-700 resize-none border border-tertiary-darker rounded-lg bg-tertiary-dark" defaultValue="Nasi Goreng&#10;Soto&#10;Bakso&#10;Sate">
+            <div className="min-h-[50vh] flex flex-col items-center sm:w-[50%] sm:items-start sm:p-5">
+                <h6 className="font-poppins font-medium text-gray-700 sm:text-lg">Daftar Makanan:</h6>
+                <textarea id="foodList" name="foodList" rows="10" placeholder="Masukkan nama-nama makanan yang dipisahkan dengan baris baru seperti:&#10;Nasi Goreng&#10;Bakso&#10;Sate&#10;Soto" className="my-3 p-2 w-[90%] font-poppins text-sm text-gray-700 resize-none border border-tertiary-darker rounded-lg bg-tertiary-dark sm:w-full sm:text-base" defaultValue="Nasi Goreng&#10;Soto&#10;Bakso&#10;Sate">
                 </textarea>
                 <Button label="Buat Roda" />
-                <h6 className="mt-3 font-poppins font-medium text-gray-700">Rekomendasi:</h6>
-                <select name="categories" id="categories" className="my-3 py-1 px-3 font-poppins text-sm text-gray-700 border border-tertiary-darker rounded-lg bg-tertiary-dark" >
+                <h6 className="mt-6 font-poppins font-medium text-gray-700 sm:text-lg">Rekomendasi:</h6>
+                <select name="categories" id="categories" className="my-3 py-1 px-3 font-poppins text-sm text-gray-700 border border-tertiary-darker rounded-lg bg-tertiary-dark sm:text-base" >
                     <option value="indonesian">Makanan Indonesia</option>
                     <option value="korean">Makanan Korea</option>
                     <option value="japanese">Makanan Jepang</option>
                 </select>
-                <Button label="Nasi Goreng" />
+                <Button label="+ Nasi Goreng" />
             </div>
         </div>
     )
