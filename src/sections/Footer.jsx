61352d04b44af8735ef0a8677855627bfb04c2a6
Andi Fikri Safaraz <afsafarazd@gmail.com>
2024-12-08 21:47:01 +0800

    Added footer

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="w-full h-20 bg-tertiary-dark font-poppins text-gray-600 text-sm text-center rounded-t-3xl flex flex-col justify-center">
+            <p>The food illustration was designed by Freepik</p>
+            <p>Copyright © 2024 Safaraz</p>
+        </div>
+    )
+}
+
+export default Footer
